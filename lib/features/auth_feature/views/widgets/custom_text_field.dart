f510b1ab22a2e13da8b6d5110f795b3add2c70bb
khaledgamal22 <kghaly56@gmail.com>
2023-09-03 04:22:19 +0300

    create custom textformfield

---
+++ lib/features/auth_feature/views/widgets/custom_text_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.headtext, required this.icon, required this.hinttext});
+
+  final String headtext;
+  final IconData icon;
+  final String hinttext;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            headtext,
+            style: TextStyle(
+              color: Color(0xff808194),
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 8,),
+          TextFormField(
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff2A2B2E),
+            ),
+            decoration: InputDecoration(
+              hintText: hinttext,
+              suffixIcon: Icon(icon,size: 20,color: Color(0xff808194),),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Color(0xff808194),),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.cyan,),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/auth_feature/views/widgets/login_view_body.dart
+++ lib/features/auth_feature/views/widgets/login_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:auth_task/features/auth_feature/views/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -8,7 +9,39 @@ class LoginViewBody extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          Image.asset('assets/images/Rectangle 1135.png'),
+          Container(
+            height: MediaQuery.sizeOf(context).height*0.3,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.75),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/Rectangle 1135.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Log in to your account',
+              style: TextStyle(
+                color: Color(0xff2A2B2E),
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          SizedBox(height: 22,),
+          CustomTextField(
+            headtext: 'Username',
+            hinttext: 'Enter your username',
+            icon: Icons.cancel_outlined,
+          ),
+          SizedBox(height: 22,),
+          CustomTextField(
+            headtext: 'Password',
+            hinttext: 'Enter your password',
+            icon: Icons.visibility_off,
+          ),
         ],
       ),
     );
